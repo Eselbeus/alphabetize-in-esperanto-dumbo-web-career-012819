37fd5ba673f2d2228a074fa82024df5de3af58fb
eselbeus <eselbeus@gmail.com>
2019-01-26 18:26:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,12 @@
 def alphabetize(arr)
   alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   binding.pry
-  res = arr.sort_by do |word|
-    word.each_char do |a| 
-      # binding.pry
-      alpha.each_char.sort_by {|b| b <=> a}
-    end
-  end
-  # code here
-  res
+  # res = arr.sort_by do |word|
+  #   word.each_char do |a| 
+  #     # binding.pry
+  #     alpha.each_char.sort_by {|b| b <=> a}
+  #   end
+  # end
+  # # code here
+  # res
 end
